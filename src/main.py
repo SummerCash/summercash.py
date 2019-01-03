2b63de52f3bc5ac4e6333463470141b0e8102b09
Dowland Aiello <mitsukomegumii@gmail.com>
2019-01-03 10:27:46 -0500

    Removed insecure channel.

--- src/main.py
+++ src/main.py
@@ -5,22 +5,13 @@ from proto.grpc import common_pb2_grpc
 from credentials import credentials
 
 def run():
-    # creds = grpc.ssl_channel_credentials(
-    #     root_certificates=None,
-    #     private_key=LoadKey(),
-    #     certificate_chain=LoadCert()
-        
-    # )
-    # # credentials = grpc.ssl_channel_credentials(LoadCert(), LoadKey())
-    
+    credentials = grpc.ssl_channel_credentials() # Get credentials
 
-    # with grpc.secure_channel(target="https://localhost:8080", credentials=credentials, options=None) as channel:
-    with grpc.insecure_channel('https://localhost:8080') as channel:
-        
-        stub = common_pb2_grpc.CommonStub(channel)
-        
-        response = stub.DecodeString(common_pb2.GeneralRequest(input=b'test', s='test'))
-    print("Client received: " + response.message)
+    with grpc.secure_channel(target="localhost:8080", credentials=credentials, options=None) as channel: # Open https channel
+        stub = common_pb2_grpc.CommonStub(channel) # Get common.proto stub
+
+        response = stub.DecodeString(common_pb2.GeneralRequest(input=b'test', s='test')) # Call
+    print("Client received: " + response.message) # Get response
 
 
 if __name__ == '__main__':
