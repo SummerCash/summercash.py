84ed18cbc200bd755879bcced4397210c6fefdcd
xoreo <mattnappo@gmail.com>
2019-01-02 22:07:08 -0500

    Using gRPC now

--- src/main.py
+++ src/main.py
@@ -1,23 +1,17 @@
-import requests
-import json
+import grpc
 
-def main():
-    url = "https://localhost:8080"
-    headers = {'content-type': 'application/json'}
+from proto.grpc import common_pb2
+from proto.grpc import common_pb2_grpc
 
-    # Example echo method
-    payload = {
-        "method": "DecodeString",
-        "params": ["hello world"],
-        "jsonrpc": "2.0",
-        "id": 0,
-    }
-    response = requests.post(
-        url, data=json.dumps(payload), headers=headers).json()
 
-    # assert response["result"] == "echome!"
-    # assert response["jsonrpc"]
-    # assert response["id"] == 0
+def run():
+    with grpc.secure_channel('https://localhost:8080') as channel:
+        stub = common_pb2_grpc.CommonStub(channel)
+        
+        response = stub.DecodeString(common_pb2.GeneralRequest(input=b'test', s='test'))
+    print("Greeter client received: " + response.message)
 
-if __name__ == "__main__":
-    main()
+
+if __name__ == '__main__':
+    run()
+# python3 -m grpc_tools.protoc -I./proto/src --python_out=./proto/build --grpc_python_out=. ./proto/src/*
