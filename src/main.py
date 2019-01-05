fcbb8f9c71a8ad991ad1dfbdacb0fc8c0a065717
xoreo <mattnappo@gmail.com>
2019-01-05 00:22:03 -0500

    Using beta implementations of grpc

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import grpc
 import os
+from grpc.beta import implementations
+
 
 class API:
     def __init__(self, rpcIP):
@@ -7,23 +9,36 @@ class API:
         self.cert = self.LoadCert("term")
         self.key = self.LoadKey("term")
 
-        self.credentials = grpc.ssl_channel_credentials(
+        # self.credentials = grpc.ssl_channel_credentials(
+        #     private_key=self.key,
+        #     certificate_chain=self.cert
+        # )
+        self.credentials = implementations.ssl_channel_credentials(
             private_key=self.key,
             certificate_chain=self.cert
         )
+        
+        
 
     def LoadCert(self, prefix):
-        with open(os.path.abspath("certs/new/" + prefix + "Cert.pem"), "rb") as f:
+        with open(os.path.abspath("../certs/new/" + prefix + "Cert.pem"), "rb") as f:
             return f.read()
 
     def LoadKey(self, prefix):
-        with open(os.path.abspath("certs/new/" + prefix + "Key.pem"), "rb") as f:
+        with open(os.path.abspath("../certs/new/" + prefix + "Key.pem"), "rb") as f:
             return f.read()
     
     def GetChannel(self):
-        with grpc.secure_channel(target=self.ip, credentials=self.credentials, options=None) as channel:
-            self.channel = channel
+        channel = grpc.secure_channel(target=self.ip, credentials=self.credentials, options=None)
+        # with grpc.secure_channel(target=self.ip, credentials=self.credentials, options=None) as channel:
+        #     self.channel = channel
+        self.channel = implementations.Channel(channel)
 
 
 if __name__ == "__main__":
-    API("localhost:8080")
+    api = API("localhost:8080")
+    api.GetChannel()
+    import common
+    _common = common.Common(api.channel)
+    response = _common.Encode(b"test", "test")
+    print(response)
