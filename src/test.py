f1dc1394f91038989a7859fbbc6c60e9413252f5
xoreo <mattnappo@gmail.com>
2019-01-09 21:00:25 -0500

    Drafted CI

--- src/test.py
+++ src/test.py
@@ -6,24 +6,110 @@ import common
 import coordinationchain
 import crypto
 import transaction
-# rpc NewAccount(GeneralRequest) returns (GeneralResponse) {} // Create new account
-#     rpc AccountFromKey(GeneralRequest) returns (GeneralResponse) {} // Generate account from given private key
-#     rpc GetAllAccounts(GeneralRequest) returns (GeneralResponse) {} // Walk keystore directory
-#     rpc MakeEncodingSafe(GeneralRequest) returns (GeneralResponse) {} // Hash specified byte array n times
-#     rpc RecoverSafeEncoding(GeneralRequest) returns (GeneralResponse) {} // Hash specified byte array n times to string
-#     rpc String(GeneralRequest) returns (GeneralResponse) {} // Hash specified byte array using sha3d algorithm
-#     rpc Bytes(GeneralRequest) returns (GeneralResponse) {} // Hash specified byte array to string using sha3d algorithm
-#     rpc ReadAccountFromMemory(GeneralRequest) returns (GeneralResponse) {} // Read account from persistent memory
+
+server = Server("")
+
 def TestAccounts():
-    server = Server("https://localhost:8080")
-    
     methods = [
-        
+        "NewAccount",
+        "AccountFromKey",
+        "GetAllAccounts",
+        "MakeEncodingSafe",
+        "RecoverSafeEncoding",
+        "String",
+        "Bytes",
+        "ReadAccountFromMemory"
     ]
-    myAccounts = accounts.Accounts(server)
-    r = myAccounts.CallMethod("NewAccount", "", "")
-    print(r)
+    for method in methods:
+        myAccounts = accounts.Accounts(server)
+        r = myAccounts.CallMethod(method, "", "")
+        print(r)
 
+def TestChain():
+    methods = [
+        "GetBalance",
+        "Bytes",
+        "String",
+        "ReadChainFromMemory",
+        "QueryTransaction",
+        "GetNumTransactions"
+    ]
+    for method in methods:
+        myChain = chain.Chain(server)
+        r = myChain.CallMethod(method, "")
+        print(r)
 
+def TestChainConfig():
+    methods = [
+        "NewChainConfig",
+        "Bytes",
+        "String",
+        "WriteToMemory",
+        "ReadChainConfigFromMemory",
+        "GetTotalSupply"
+    ]
+    for method in methods:
+        myChainConfig = chainconfig.ChainConfig(server)
+        r = myChainConfig.CallMethod(method, "")
+        print(r)
+
+def TestCommon():
+    methods = [
+        "Encode",
+        "EncodeString",
+        "Decode",
+        "DecodeString"
+    ]
+    for method in methods:
+        myCommon = common.Common(server)
+        r = myCommon.CallMethod(method, "", "")
+        print(r)
+
+def TestCoordinationChain():
+    methods = [
+        "SyncNetwork",
+        "GetPeers",
+        "Bytes",
+        "String"
+    ]
+    for method in methods:
+        myCoordinationChain = coordinationchain.CoordinationChain(server)
+        r = myCoordinationChain.CallMethod(method)
+        print(r)
+
+def TestCrypto():
+    methods = [
+        "Sha3",
+        "Sha3String",
+        "Sha3n",
+        "Sha3nString",
+        "Sha3d",
+        "Sha3dString"
+    ]
+    for method in methods:
+        myCrypto = crypto.Crypto(server)
+        r = myCrypto.CallMethod(method, "", "")
+        print(r)
+
+def TestTransaction():
+    methods = [
+        "NewTransaction",
+        "TransactionFromBytes",
+        "Publish",
+        "Bytes",
+        "String",
+        "SignTransaction",
+        "VerifyTransactionSignature"
+    ]
+    for method in methods:
+        myTransaction = transaction.Transaction(server)
+        r = myTransaction.CallMethod(method, "", "", "", "", "")
+        print(r)
 
 TestAccounts()
+TestChain()
+TestChainconfig()
+TestCommon()
+TestCoordinationChain()
+TestCrypto()
+TestTransaction()
