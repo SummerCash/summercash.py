d22991850110c2481bebbcee734d5893a6fc4552
Dowland Aiello <mitsukomegumii@gmail.com>
2019-01-09 20:29:19 -0500

    :facepalm: have to get JSON response value first, though.

--- src/common/common.py
+++ src/common/common.py
@@ -3,9 +3,9 @@ RequestShouldVerify = False # Global definition for verify= param
 
 # GetRequestResponse - determine proper response, return it
 def GetRequestResponse(request):
-    if "msg" in request.keys(): # Check error
-        return request['msg'] # Return response
-    elif "message" in request.keys(): # Check success
-        return request['message'] # Return response
+    if "msg" in request.json().keys(): # Check error
+        return request.json()['msg'] # Return response
+    elif "message" in request.json().keys(): # Check success
+        return request.json()['message'] # Return response
     else:
-        return request['meta'] # Return RPC error
+        return request.json()['meta'] # Return RPC error
